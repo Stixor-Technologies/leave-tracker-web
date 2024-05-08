9fa17f038ce24aeda2c7f5d25592ef482bee09d4
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-05-08 13:30:19 +0500

    added api slice in store

--- redux/store.ts
+++ redux/store.ts
@@ -2,6 +2,7 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { userSlice } from "./slice/user-slice";
+import { authApi } from "./apis/auth-api";
 
 const persistConfig = {
   key: "root",
@@ -10,13 +11,15 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userSlice.reducer,
+  [authApi.reducerPath]: authApi.reducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
 });
 
 export const persistor = persistStore(store);
