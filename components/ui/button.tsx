904f7a0355f2219a3a6657cf518c4b14d40364db
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-05-11 15:55:41 +0500

    changed the outline color of button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -8,7 +8,7 @@ import { RotateCw } from "lucide-react";
 const generalClasses = "h-10 px-4 py-2 w-full";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
