ccbdef7c28a432537278c125a9b2f89f0338ebed
imrankhan-stixor <154209226+imrankhan-stixor@users.noreply.github.com>
2024-05-14 11:56:52 +0500

    replaced functional based reusable approach with normal buttons usage

--- components/social-sign-up.tsx
+++ components/social-sign-up.tsx
@@ -7,24 +7,24 @@ import { useRouter } from "next/navigation";
 const SocialSignUp = ({ title = "signup" }: { title?: string }) => {
   const router = useRouter();
 
-  useEffect(() => router.refresh(), []);
+  // useEffect(() => router.refresh(), []);
 
-  const socialLink = (icon: ReactNode, name: string, link: string) => {
-    return (
-      <Button
-        variant={"transparent"}
-        className="transition-background flex grow items-center justify-center rounded-md border px-4 py-2 duration-300 hover:bg-slate-100"
-        onClick={() => {
-          router.push(link);
-        }}
-      >
-        <span className="flex items-center justify-center gap-[0.5rem]">
-          {icon}
-          <span className="text-sm font-medium">{name}</span>
-        </span>
-      </Button>
-    );
-  };
+  // const socialLink = (icon: ReactNode, name: string, link: string) => {
+  //   return (
+  //     <Button
+  //       variant={"transparent"}
+  //       className="transition-background flex grow items-center justify-center rounded-md border px-4 py-2 duration-300 hover:bg-slate-100"
+  //       onClick={() => {
+  //         router.push(link);
+  //       }}
+  //     >
+  //       <span className="flex items-center justify-center gap-[0.5rem]">
+  //         {icon}
+  //         <span className="text-sm font-medium">{name}</span>
+  //       </span>
+  //     </Button>
+  //   );
+  // };
 
   return (
     <div className="flex flex-col gap-6">
@@ -36,6 +36,42 @@ const SocialSignUp = ({ title = "signup" }: { title?: string }) => {
       </div>
 
       <div className="grid grid-cols-1 gap-[0.625rem] sm:grid-cols-2">
+        <Button
+          variant={"transparent"}
+          className="transition-background flex grow items-center justify-center rounded-md border px-4 py-2 duration-300 hover:bg-slate-100"
+          onClick={() => {
+            router.push(SOCIAL_LINKS.GOOGLE);
+          }}
+        >
+          <span className="flex items-center justify-center gap-[0.5rem]">
+            <Image
+              src={"/assets/images/social-icons/google-icon.svg"}
+              alt="google icon"
+              width={18}
+              height={18}
+            />
+            <span className="text-sm font-medium">Google</span>
+          </span>
+        </Button>
+        <Button
+          variant={"transparent"}
+          className="transition-background flex grow items-center justify-center rounded-md border px-4 py-2 duration-300 hover:bg-slate-100"
+          onClick={() => {
+            router.push(SOCIAL_LINKS.SLACK);
+          }}
+        >
+          <span className="flex items-center justify-center gap-[0.5rem]">
+            <Image
+              src={"/assets/images/social-icons/slack-icon.svg"}
+              alt="slack icon"
+              width={18}
+              height={18}
+            />
+            <span className="text-sm font-medium">Slack</span>
+          </span>
+        </Button>
+      </div>
+      {/* <div className="grid grid-cols-1 gap-[0.625rem] sm:grid-cols-2">
         {socialLink(
           <Image
             src={"/assets/images/social-icons/google-icon.svg"}
@@ -57,7 +93,7 @@ const SocialSignUp = ({ title = "signup" }: { title?: string }) => {
           "Slack",
           SOCIAL_LINKS.SLACK,
         )}
-      </div>
+      </div> */}
     </div>
   );
 };
