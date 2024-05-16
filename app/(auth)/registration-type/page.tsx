ffda735e022356a8c88c7d7bdccd74d14429ce55
fawadmehmood1 <fawad.mehmood@stixor.com>
2024-05-16 15:55:36 +0500

    fixes styling on auth form

--- app/(auth)/registration-type/page.tsx
+++ app/(auth)/registration-type/page.tsx
@@ -58,7 +58,7 @@ const RegisterationType: NextPage = () => {
         Embrace My Leave Tracker as your go-to solution for:
       </h1>
 
-      <div className="!flex flex-col items-center gap-8 md:flex-row">
+      <div className="!flex flex-col items-center justify-center gap-8 md:flex-row">
         <Link href={ROUTES.ORGANIZATION_REGISTRATION}>
           {card(
             organizationImage,
